a1a131717da9991c7a6fc7aaff57ff9f972ff221
tjava <mustyytee@gmail.com>
2022-02-05 13:49:48 +0100

    url bug fixed

--- hospitals/views.py
+++ hospitals/views.py
@@ -6,15 +6,15 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-# from .filters import HospitalsFilter
+from .filters import HospitalsFilter
 from .models import Hospital
 from .serializers import HospitalSerializer
 
 
 class HospitalViewSet(viewsets.ModelViewSet):
-    queryset = Hospital.objects.all()[:20]
+    queryset = Hospital.objects.all()
     serializer_class = HospitalSerializer
-    # filter_class = HospitalsFilter
+    filter_class = HospitalsFilter
     filter_backends = (DjangoFilterBackend,)
 
     @action(detail=False, methods=["get"])
@@ -27,7 +27,7 @@ class HospitalViewSet(viewsets.ModelViewSet):
             user_location = Point(float(longitude), float(latitude), srid=4326)
             closest_hospitals = Hospital.objects.filter(
                 geom__distance_lte=(user_location, D(km=3))
-            )[:20]
+            )
             serializer = self.get_serializer_class()
             serialized_hospitals = serializer(closest_hospitals, many=True)
             return Response(serialized_hospitals.data, status=status.HTTP_200_OK)
